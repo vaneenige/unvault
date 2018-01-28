892eee9577e06a3827f8f45d3cb6b5879a05a362
Colin van Eenige <cvaneenige@gmail.com>
2018-01-28 23:16:44 +0100

    Update benchmark name.

--- bench/mongo.js
+++ bench/mongo.js
@@ -5,14 +5,16 @@ const unvault = require("./../lib");
 
 const routes = unvault();
 
-// Helper function to connect with mongodb
 function connect(callback) {
-  mongodb.MongoClient.connect(`mongodb://localhost:27017/cms`, (error, db) =>
-    callback(error !== null ? error : db)
-  );
+  mongodb.MongoClient.connect(`mongodb://localhost:27017/cms`, (error, db) => {
+    if (error !== null) {
+      console.log(error);
+    } else {
+      callback(db);
+    }
+  });
 }
 
-// Helper function to the nodes from mongodb
 async function getNodes(db) {
   return db
     .collection("nodes")
@@ -21,17 +23,14 @@ async function getNodes(db) {
 }
 
 connect(db => {
-  // Inserts a tracker into the vault
   routes.insert("mongo", 2000, async () => {
     const nodes = await getNodes(db);
     return JSON.stringify(nodes);
   });
 
-  // Start the Polka node server
   const server = polka();
   server.listen(3000);
 
-  // Listen to the route that equals the tracker key
   server.get("/slow/mongo", async (req, res) => {
     const nodes = await getNodes(db);
     send(res, 200, JSON.stringify(nodes), {
